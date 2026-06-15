ca72303e7c21b137a61512aa9d3710fbbc0dee5c
jeremy <jeremy.chaufourier@opsone.net>
2026-06-15 23:09:51 +0200

    docs: cleanUrls

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   titleTemplate: ':title | Catapult',
   description: siteDescription,
   lang: 'en-US',
+  cleanUrls: true,
   sitemap: {
     hostname: siteUrl,
   },
@@ -26,6 +27,8 @@ export default defineConfig({
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:site_name', content: 'Catapult' }],
     ['meta', { property: 'og:image', content: `${siteUrl}/og.png` }],
+    ['meta', { property: 'og:image:width', content: '1200' }],
+    ['meta', { property: 'og:image:height', content: '630' }],
     // Twitter Card
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { name: 'twitter:image', content: `${siteUrl}/og.png` }],
