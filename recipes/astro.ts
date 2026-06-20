41a2d58fbba872d61055a19e822f6ed21f4c774f
Jeremy <jeremy@chaufourier.fr>
2026-06-20 14:55:29 +0200

    refacto: astro recipe

--- recipes/astro.ts
+++ recipes/astro.ts
@@ -3,21 +3,16 @@
  * DESCRIPTION:
  * This recipe builds a local project then transfers files to a remote server using scp.
  */
-import { type TaskContext, task, desc, local, upload, get, set, before } from '../index.ts'
+import { type TaskContext, task, desc, local, get, set, before } from '../index.ts'
 import { resolveHostStringValue } from '../src/utils.ts'
 
 set('astro_mode', 'production')
+set('source_path', './dist/.')
 
 desc('Builds the Astro application locally')
 task('deploy:build', async ({ host }: TaskContext) => {
   const mode = resolveHostStringValue(get('astro_mode'), host, 'astro_mode')
   await local(`astro build --mode ${mode}`)
-})
-
-desc('Uploads local artifacts to the release directory')
-task('deploy:update_code', async ({ paths }: TaskContext) => {
-  const source = get('source_path', './dist/.')
-  await upload(source, paths.release)
 })
 
 before('deploy:lock', 'deploy:build')
