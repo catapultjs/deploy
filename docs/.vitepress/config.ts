bbaf0aa044d586fe44b934666ca4e592f2eb74e5
Jeremy <jeremy@chaufourier.fr>
2026-06-20 19:39:01 +0200

    feat: add favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
     hostname: siteUrl,
   },
   head: [
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     [
       'script',
       {
