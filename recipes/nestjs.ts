888533b4da4b8cad7315fe471a8a026208c48538
Jeremy <jeremy@chaufourier.fr>
2026-06-20 16:07:48 +0200

    chore: update header nestjs recipe

--- recipes/nestjs.ts
+++ recipes/nestjs.ts
@@ -1,3 +1,8 @@
+/**
+ * TYPE: remote-build
+ * DESCRIPTION:
+ * This recipe builds and deploys a NestJS app directly on the remote server.
+ */
 import { set, after } from '../index.ts'
 
 set('shared_files', ['.env'])
